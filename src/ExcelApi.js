c72832d8d9056399f0dbb778bfaebbd4144b31f2
JohnathonLamOF <johnathon@openfin.co>
2018-10-05 14:36:05 +0100

    Fixed excel connection

--- src/ExcelApi.js
+++ src/ExcelApi.js
@@ -172,11 +172,7 @@ class ExcelService extends RpcDispatcher_1.RpcDispatcher {
      */
     configureDefaultApplication() {
         return __awaiter(this, void 0, void 0, function* () {
-            if (!this.defaultApplicationObj) {
-                console.error('No default applciation object');
-                return;
-            }
-            const defaultAppObjUuid = this.defaultApplicationObj.connectionUuid;
+            const defaultAppObjUuid = this.defaultApplicationObj && this.defaultApplicationObj.connectionUuid;
             const defaultAppEntry = this.mApplications[defaultAppObjUuid];
             const defaultAppObjConnected = defaultAppEntry ? defaultAppEntry.connected : false;
             if (defaultAppObjConnected) {
